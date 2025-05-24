faba8a07b745f5d84ebbe721ca9b40ac008b84e7
Prateek Singh <153483614+Ashuair@users.noreply.github.com>
2025-05-24 23:08:01 +0530

    Update Hello.java

--- src/Java_Basics_01/Hello.java
+++ src/Java_Basics_01/Hello.java
@@ -2,6 +2,6 @@ package Java_Basics_01;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 }
