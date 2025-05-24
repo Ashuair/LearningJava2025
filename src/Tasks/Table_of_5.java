1b64f8d107803ec3cb8c68b299b70c07c490e12b
Ashuair <153483614+Ashuair@users.noreply.github.com>
2025-05-25 01:02:47 +0530

    commit

---
+++ src/Tasks/Table_of_5.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+
+// Write table of 5 like this 5 x 1 = 5
+public class Table_of_5 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("5 x 1 =%d ", num);
+        System.out.printf('\n'+"5 x 2 =%d ", num*2);
+        System.out.printf('\n'+"5 x 3 =%d ", num*3);
+        System.out.printf('\n'+"5 x 4 =%d ", num*4);
+        System.out.printf('\n'+"5 x 5 =%d ", num*5);
+        System.out.printf('\n'+"5 x 6 =%d ", num*6);
+        System.out.printf('\n'+"5 x 7 =%d ", num*7);
+        System.out.printf('\n'+"5 x 8 =%d ", num*8);
+        System.out.printf('\n'+"5 x 9 =%d ", num*9);
+        System.out.printf('\n'+"5 x 10 =%d ", num*10);
+
+
+    }
+}
