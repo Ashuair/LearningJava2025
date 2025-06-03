c2ba43de9580ea2c6aa7a196cb5e8760427dfc87
Ashuair <153483614+Ashuair@users.noreply.github.com>
2025-06-03 23:32:13 +0530

    commit

---
+++ src/String_Tasks/Reverse_StringBuffer.java
@@ -0,0 +1,8 @@
+package String_Tasks;
+
+public class Reverse_StringBuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello Prateek");
+        System.out.println(sb.reverse());
+    }
+}
