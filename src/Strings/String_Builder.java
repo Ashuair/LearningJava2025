87385ec107607313ee1e656aaf9f932d3988c0a6
Ashuair <153483614+Ashuair@users.noreply.github.com>
2025-06-03 22:50:14 +0530

    commit

---
+++ src/Strings/String_Builder.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class String_Builder {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Prateek");
+        sb.append(" Hello");
+        System.out.println(sb.reverse());
+    }
+}
